e8cca565902f099e79df9ee3ec147d9a73d2b867
Vasya999999 <84250405+Vasya999999@users.noreply.github.com>
2024-01-17 04:38:02 +0200

    Update change_data.sql

--- Laboratory-work-8/change_data.sql
+++ Laboratory-work-8/change_data.sql
@@ -1,1 +1,13 @@
-
+CREATE OR REPLACE FUNCTION get_order_info(order_name VARCHAR)
+RETURNS TABLE
+(id INTEGER, name VARCHAR, info VARCHAR)
+AS $$
+DECLARE
+	str VARCHAR;
+BEGIN
+	str := 'SELECT id, name, info from order_info where name = ''' || 
+		order_name || '''';
+	RAISE NOTICE 'Query=%',str;
+	RETURN QUERY EXECUTE str;
+END;
+$$ LANGUAGE plpgsql;
